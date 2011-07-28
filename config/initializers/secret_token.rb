61460a06d8c41bbc1facdad847b1b1d0aadd4653
Chad Pierce <chad@attitude-graphics.com>
2011-07-27 22:17:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '90ddc0e947e8896755a9e072a8c560f99a9207f8a9583286794de388a54f9eba466d7eeff9315aa6e85952e41d9f4e5a347ac4ce93a188274d2d3a33c0361c8e'
